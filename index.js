8de8de77d59442eacf8682353860243a92846ef1
Megan Horsfield <meganthadev@gmail.com>
2020-08-06 04:30:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function createEmlpoyeeReccord(empInfo){
   let empRecord = {
-    firstName: empInfo[0]
-    
-  }
+    firstName: empInfo[0],
+    familyName: empInfo[1],
+    title: empInfo[2],
+    payPerHour: empInfo[3]
+    timeInEvents = []
+    timeOutEvents = []  }
 }
